167b579ca9eddb58e701f0694c8878a9a1a2706c
Gianluca Casati <fibo@users.noreply.github.com>
2026-02-13 19:55:04 +0700

    add extract to generalize extractors

--- haicu.js
+++ haicu.js
@@ -5,71 +5,53 @@ const CLOSE_TAG = '>'
 const CLOSING_TAG = '/'
 const ESCAPE = "'"
 
-export const extractEscaped = arg => arg.matchAll(RegExp(
-	`(?<before>[^${ESCAPE}]*)` +
-	'(:?' +
+const extract = (startSymbol, matcher, resolver) => arg =>
+	typeof arg !== 'string' ? arg : arg.matchAll(RegExp(
+	`(?<before>[^${startSymbol}]*)` +
+	`(:?${matcher}` +
+	`(?<after>[^${startSymbol}]*)` +
+	'){0,1}',
+'g')).reduce((tokens, { groups: { before, after, ...rest } = {} }) =>
+	tokens.concat(
+		[before, resolver(rest), after]
+		.filter(x => !!x)
+	)
+, [])
+
+export const extractEscaped = extract(
+	ESCAPE,
 	`${ESCAPE}(?:` +
 		`(?<escapedPart>\\${OPEN_BRACKET}[^${CLOSE_BRACKET}${ESCAPE}]*\\${CLOSE_BRACKET})${ESCAPE}` +
 		`|(?<doubleQuote>${ESCAPE})` +
-	`)` +
-	`(?<after>[^${ESCAPE}]*)` +
-	'){0,1}',
-'g'))
-	.reduce((tokens, { groups = {} }) => {
-		const { before, escapedPart, doubleQuote, after } = groups
-		if (before)
-			tokens.push(before)
+	')',
+	({ escapedPart, doubleQuote }) => {
 		if (escapedPart)
-			tokens.push({ type: 'text', text: escapedPart })
-		else if (doubleQuote)
-			tokens.push(ESCAPE)
-		if (after)
-			tokens.push(after)
-		return tokens
-	}, [])
+			return { type: 'text', text: escapedPart }
+		if (doubleQuote)
+			return ESCAPE
+	}
+)
 
-export const extractArg = arg => arg.matchAll(RegExp(
-	`(?<before>[^${OPEN_BRACKET}]*)` +
-	'(:?' +
-	`\\${OPEN_BRACKET}(?<messageArg>[^${CLOSE_BRACKET}]*)\\${CLOSE_BRACKET}` +
-	`(?<after>[^${OPEN_BRACKET}]*)` +
-	'){0,1}',
-'g'))
-	.reduce((tokens, { groups = {} }) => {
-		const { before, messageArg, after } = groups
-		if (before)
-			tokens.push(before)
-		if (messageArg)
-			tokens.push({ type: 'arg', arg: messageArg })
-		if (after)
-			tokens.push(after)
-		return tokens
-	}, [])
+export const extractArg = extract(
+	OPEN_BRACKET,
+	`\\${OPEN_BRACKET}(?<messageArg>[^${CLOSE_BRACKET}]*)\\${CLOSE_BRACKET}`,
+	({ messageArg }) =>
+		messageArg && { type: 'arg', arg: messageArg }
+)
 
-export const extractTag = arg => arg.matchAll(RegExp(
-	`(?<before>[^${OPEN_TAG}]*)` +
-	'(:?' +
-	`${OPEN_TAG}(?<isClosing>${CLOSING_TAG})?(?<tag>[^${CLOSE_TAG}]+)${CLOSE_TAG}` +
-	`(?<after>[^${OPEN_TAG}]*)` +
-	'){0,1}',
-'g'))
-	.reduce((tokens, { groups = {} }) => {
-		const { before, isClosing, tag, after } = groups
-		if (before)
-			tokens.push(before)
-		if (tag)
-			tokens.push({ type: isClosing ? 'closeTag' : 'openTag', tag })
-		if (after)
-			tokens.push(after)
-		return tokens
-	}, [])
+export const extractTag = extract(
+	OPEN_TAG,
+	`${OPEN_TAG}(?<isClosing>${CLOSING_TAG})?(?<tag>[^${CLOSE_TAG}]+)${CLOSE_TAG}`,
+	({ tag, isClosing }) =>
+		tag && { type: isClosing ? 'closeTag' : 'openTag', tag }
+)
 
 export const lexer = arg => extractEscaped(arg)
 	.reduce((tokens, result) => tokens.concat(
-		typeof result === 'string' ? extractTag(result) : result
+		extractTag(result)
 	), [])
 	.reduce((tokens, result) => tokens.concat(
-		typeof result === 'string' ? extractArg(result) : result
+		extractArg(result)
 	), [])
 	.reduce((tokens, result) => tokens.concat(
 		typeof result === 'string' ? ({ type: 'text', text: result }) : result
